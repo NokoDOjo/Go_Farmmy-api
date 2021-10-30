9781424491d4a78acf4f030ce91f169f66ee79a7
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-10-30 20:13:07 +0800

    feat: init testing docs

---
+++ test/requests/product.spec.js
@@ -0,0 +1,39 @@
+const sinon = require('sinon')
+const chai = require('chai')
+const request = require('supertest')
+chai.use(require('sinon-chai'))
+const db = require('../../models')
+const app = require('../../app')
+
+const { expect } = require('chai')
+const { sequelize, dataTypes, checkModelName, checkUniqueIndex, checkPropertyExists } = require('sequelize-test-helpers')
+
+describe('#Product requests', () => {
+  describe('GET /products', () => {
+    before(async function() {
+      await db.Product.destroy({ where: {}, truncate: true })
+
+      await db.Product.create({ name: 'product1', quantity: 10 })
+      await db.Product.create({ name: 'product2', quantity: 10 })
+    })
+
+    it('GET all products', done => {
+      request(app)
+        .get('/api/products')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err)
+          expect(res.body.products.length).to.be.equal(2)
+          expect(res.body.products[0].name).to.be.equal('product1')
+          expect(res.body.products[1].name).to.be.equal('product2')
+
+          done()
+        })
+    })
+
+    after(async function() {
+      await db.Product.destroy({ where: {}, truncate: true })
+    })
+  })
+})
